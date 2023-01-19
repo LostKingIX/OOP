db218db1065dead1144c853eff228acb29e83cf1
Kuldeep M <kuldeepmohanta99@gmail.com>
2023-01-19 12:01:45 -0500

    In class Examples

---
+++ OOP/Class Examples/ExampleIO.java
@@ -0,0 +1,23 @@
+import java.text.Format;
+import java.util.Scanner;
+
+public class ExampleIO() {
+    public static void main(String[] args) {
+        
+        //Initializing Scanner for system input
+        Scanner scanner = new Scanner(System.in);
+
+        String firstName, lastName, fullName;
+
+        System.out.print("Enter your Full name: ");
+        fullName = scanner.next();
+
+        scanner.nextLine();
+
+        System.out.printf(Format: "Hello Mr. %s, nice to meet you!\n");
+
+        System.out.print("Press [Enter] to exit: ");
+
+        scanner.close();
+    }
+}
