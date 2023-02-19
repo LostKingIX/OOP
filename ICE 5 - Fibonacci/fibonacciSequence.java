0b1ebf4b12e9c5c45b4a2566ea2838f610b4b423
LostKingIX <kuldeepmohanta99@gmail.com>
2023-02-18 23:56:59 -0500

    Finished ICE 5

--- ICE 5 - Fibonacci/fibonacciSequence.java
+++ ICE 5 - Fibonacci/fibonacciSequence.java
@@ -4,7 +4,12 @@ import java.util.Scanner;
  * Author: Kuldeep Mohanta
  * Date: Feb 16th, 2023
  * App Name: Fibonacci Sequence
- * Description: 
+ * Description: This application is designed to prompt the user to input a number that will be used to determine
+ * a index range to calculate the fibonacci sequence until. This sequence is used within this application through the 
+ * implentation of recursion - within the askforMax() function. Recursion here functions by calling a method
+ * within itself, which in this case if the user does not enter a number that passes validation then the method
+ * will call itself recursively until the input is valid.
+ * 
  */
 
  public class fibonacciSequence {
