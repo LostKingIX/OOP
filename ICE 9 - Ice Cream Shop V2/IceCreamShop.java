69f219d65ec3c5e19f2180a0c7517faa6cb7d1fd
Kuldeep M <kuldeepmohanta99@gmail.com>
2023-03-22 23:16:43 -0400

    New ICE 9 Files

---
+++ ICE 9 - Ice Cream Shop V2/IceCreamShop.java
@@ -0,0 +1,39 @@
+/* 
+ * Name: Kuldeep Mohanta
+ * Date: March 22, 2023
+ * App Name: Ice Cream Shop (Updated)
+ * Description: Interactive Menu of an Ice Cream Shop
+ * 
+ */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class IceCreamShop
+{
+    //Constants
+    static final String CLEAR_TERMINAL = "\033c";
+    static final String SET_TITLE = "\033]0;%s\007";
+    static final String BANNER = """
+        _
+        ,' `,.
+        >-.(__)
+       (_,-' |
+         `.  |
+           `.| Ice Cream Shop PRIME
+             `
+        """;
+
+    public static void main(String[] args)
+    {
+        // User input
+        Scanner Scanner = new Scanner (System.in);
+        
+        // An ArrayList of IceCream Flavours
+        // Start with zero elements
+        ArrayList<IceCream> flavours = new ArrayList<IceCream>();
+
+
+
+    }
+}
